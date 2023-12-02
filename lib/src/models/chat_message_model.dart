2de421f617f4944d8965f9360be4ce0defdc674b
Hrithik <iizhrithik@gmail.com>
2023-12-02 13:26:04 +0530

    Renamed ChatMessageModel

--- lib/src/models/chat_message_model.dart
+++ lib/src/models/chat_message_model.dart
@@ -1,12 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ChatMessage {
+class ChatMessageModel {
   String message;
   Timestamp timestamp;
   String userType;
   String chatID;
 
-  ChatMessage({
+  ChatMessageModel({
     required this.message,
     required this.timestamp,
     required this.userType,
@@ -14,8 +14,8 @@ class ChatMessage {
   });
 
   // Convert a map to a ChatMessage object
-  factory ChatMessage.fromMap(Map<String, dynamic> map) {
-    return ChatMessage(
+  factory ChatMessageModel.fromMap(Map<String, dynamic> map) {
+    return ChatMessageModel(
       message: map['message'],
       timestamp: map['timestamp'] as Timestamp,
       userType: map['userType'],
